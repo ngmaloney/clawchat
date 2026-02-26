3b6325712655ef39e60d958443c1fa720f26161a
Pinchy <pinchy@clawmail.dev>
2026-02-26 00:15:50 +0000

    Revert "debug: log tray icon path and existence to stderr on startup"
    This reverts commit a80d9ee2d4420b5ad6c22a72721e9a8638d9ed75.

--- electron/main.ts
+++ electron/main.ts
@@ -2,7 +2,6 @@ import { app, BrowserWindow, ipcMain, dialog, globalShortcut, Menu, Tray } from 
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import fs from 'node:fs/promises'
-import { existsSync } from 'node:fs'
 
 import Store from 'electron-store'
 import contextMenu from 'electron-context-menu'
@@ -112,16 +111,10 @@ let tray: Tray | null = null
 let isQuitting = false
 
 function createTray() {
+  // macOS requires a Template Image: black on transparent, filename ends in "Template"
+  // Both 1x (trayIconTemplate.png) and 2x (trayIconTemplate@2x.png) must exist
   const iconPath = path.join(process.env.VITE_PUBLIC!, 'trayIconTemplate.png')
-  process.stderr.write(`[Tray] icon path: ${iconPath}\n`)
-  process.stderr.write(`[Tray] file exists: ${existsSync(iconPath)}\n`)
-  try {
-    tray = new Tray(iconPath)
-    process.stderr.write('[Tray] created ok\n')
-  } catch (e) {
-    process.stderr.write(`[Tray] FAILED: ${e}\n`)
-    return
-  }
+  tray = new Tray(iconPath)
   tray.setToolTip('ClawChat')
 
   const buildContextMenu = () => Menu.buildFromTemplate([
